eb1345050ee06fcb60319e3718ed9c5021fafd89
Robert <rob89porto@gmail.com>
2020-10-16 14:09:06 -0400

    first commit

---
+++ src/main/java/com/web/repo/DaoContract.java
@@ -0,0 +1,19 @@
+package com.web.repo;
+
+import java.util.List;
+
+public interface DaoContract<T, I> {
+
+	List<T> findAll();
+
+	T findById(I i);
+
+	int update(T t);
+
+	int create(T t);
+
+	int delete(I i);
+
+	T findByName(String name);
+
+}
---
+++ src/main/java/com/web/util/ConnectionUtil.java
@@ -0,0 +1,34 @@
+package com.web.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+	{
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String url = System.getenv("project1_url");
+	private String password = System.getenv("password");
+	private String username = System.getenv("username");
+	private static ConnectionUtil instance;
+
+	private ConnectionUtil() {
+	}
+
+	public static ConnectionUtil getInstance() {
+		if (instance == null) {
+			instance = new ConnectionUtil();
+		}
+		return instance;
+	}
+
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, username, password);
+	}
+}
