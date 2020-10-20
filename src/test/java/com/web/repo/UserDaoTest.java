6fc7ec4a90759b9571b3e12fd503c86170583318
Robert <rob89porto@gmail.com>
2020-10-19 20:14:45 -0400

    adding target dir

--- src/test/java/com/web/repo/UserDaoTest.java
+++ src/test/java/com/web/repo/UserDaoTest.java
@@ -1,33 +1,23 @@
 package com.web.repo;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-
-import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import com.web.model.User;
-
 public class UserDaoTest {
-	private UserDao ud;
-
-	@Before
-	public void setup() {
-		ud = new UserDao();
-	}
-
-	@Test
-	public void findAllTest() {
-		List<User> users = ud.findAll();
-		assertFalse(users.size() == 0);
-	}
-
-	@Test
-	public void findByIdTest() {
-		User u = ud.findById(1);
-		assertNotNull(u);
-	}
+//	private UserDao ud;
+//
+//	@Before
+//	public void setup() {
+//		ud = new UserDao();
+//	}
+//
+//	@Test
+//	public void findAllTest() {
+//		List<User> users = ud.findAll();
+//		assertFalse(users.size() == 0);
+//	}
+//
+//	@Test
+//	public void findByIdTest() {
+//		User u = ud.findById(1);
+//		assertNotNull(u);
+//	}
 
 }
