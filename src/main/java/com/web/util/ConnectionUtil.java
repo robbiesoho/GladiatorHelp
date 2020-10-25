31c788523d2a4f3066584fb1f7a545bf148c36da
Robert <rob89porto@gmail.com>
2020-10-25 14:41:45 -0400

    changed db config to make it easiser for now

--- src/main/java/com/web/util/ConnectionUtil.java
+++ src/main/java/com/web/util/ConnectionUtil.java
@@ -13,9 +13,14 @@ public class ConnectionUtil {
 		}
 	}
 
-	private String url = System.getenv("url");
-	private String password = System.getenv("password");
-	private String username = System.getenv("username");
+//	private String url = System.getenv("url");
+//	private String password = System.getenv("password");
+//	private String username = System.getenv("username");
+
+//	private String testUrl = "jdbc:postgresql://portodb.czsfcl0hvotw.us-east-2.rds.amazonaws.com:5432/postgres?currentSchema=project1test_schema";
+	private String url = "jdbc:postgresql://portodb.czsfcl0hvotw.us-east-2.rds.amazonaws.com:5432/postgres?currentSchema=project1_schema";
+	private String password = "porto";
+	private String username = "roberto";
 	private static ConnectionUtil instance;
 
 	private ConnectionUtil() {
